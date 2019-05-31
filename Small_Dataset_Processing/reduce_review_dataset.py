4392178840ceb9c2f57239d0cde9266afd93a81d
Aanya Jhaveri <aanya@Aanyas-MacBook-Pro.local>
2019-05-31 18:20:15 -0500

    new dicts

--- Small_Dataset_Processing/reduce_review_dataset.py
+++ Small_Dataset_Processing/reduce_review_dataset.py
@@ -5,14 +5,21 @@ with open ('business_edited.json') as b:
     data = json.load(b)
     businesses = list(data.keys())
 
-with open('../review.json') as f:
-    data = {}
-    for line in f:
-        data_rev = json.loads(line)
-        if data_rev["business_id"] in businesses:
-            data[data_rev["review_id"]] = {"business_id": data_rev["business_id"],
+with open('../review.json', 'r') as fin:
+    with open('reviews_edited.json', 'w') as fout:
+        data = {}
+        for line in fin:
+            data_rev = json.loads(line)
+            if data_rev["business_id"] in businesses:
+                data[data_rev["review_id"]] = {"business_id": data_rev["business_id"],
                                            "user_id": data_rev["user_id"],
-                                           "stars": data_rev["stars"],
-                                           "text": data_rev["stars"]}
-with open('reviews_edited.json', 'w') as outfile:
+                                            "stars": data_rev["stars"],
+                                           "text": data_rev["text"]}
+                review_info = {"review_id":data_rev["review_id"],
+                          "business_id": data_rev["business_id"],
+                           "user_id": data_rev["user_id"],
+                            "stars": data_rev["stars"],
+                            "text": data_rev["text"]}
+                json.dump(review_info, fout)
+with open('reviews_edited_dict.json', 'w') as outfile:
     json.dump(data, outfile)

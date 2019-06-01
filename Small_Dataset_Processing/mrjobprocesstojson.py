a7434419f3332d4a799a3653322b40872b5672a8
Aanya Jhaveri <aanya@Aanyas-MacBook-Pro.local>
2019-06-01 15:38:59 -0500

    tfidf files

--- Small_Dataset_Processing/mrjobprocesstojson.py
+++ Small_Dataset_Processing/mrjobprocesstojson.py
@@ -3,20 +3,17 @@ Conversion code to transfer mrjob output into a json of words with unique
 numerical 'key' to be referenced in tf/idf vectors.
 '''
 
-# DOESNT WORK!!
 import json
 
 def mrtransfer(infile, outfile):
     with open(outfile, 'w') as fout:
         with open(infile, 'r') as fin:
-            counter = 0
             d = {}
             for line in fin:
                 word_key = tuple(line.split())
                 if len(word_key) == 2:
                     k, v = word_key
-                    d[k[1:-1]] = counter
-                    counter += 1
+                    d[k[1:-1]] = v
             json.dump(d, fout)
 
-mrtransfer('vocab_small.json', 'vocab_small_dict.json')
+mrtransfer('revfreq_smallmrjob.json', 'rev_freq.json')

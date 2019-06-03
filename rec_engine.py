1a3a19a8fc49f50dc07762134afb7a5e829becf3
Samantha Stagg <staggsamantha@cs.uchicago.edu>
2019-06-03 13:07:25 -0500

    fixed problems with rec engine output

--- rec_engine.py
+++ rec_engine.py
@@ -54,36 +54,61 @@ def create_rec_list(user_id, tfidf_file, num_recs):
     return rec_list 
 
 
-def recommend(list_of_tups):
+def recommend(list_of_tups, num_recs):
     with open('Small_Dataset_Processing/smaller_review_index_dict.json', 'r') as f:
         review_index_dict = json.load(f)
     with open('reviewid_businessid_dict.json', 'r') as g:
         business_id_dict = json.load(g)
     with open('businessid_business_name_dict.json', 'r') as h:
         business_name_dict = json.load(h)
+
+    list_of_lists = []
+    for i in range(0, int(len(list_of_tups)/num_recs)):
+        interval = num_recs*i
+        list_of_lists.append(list_of_tups[interval:interval+num_recs])
+
     liked_review_ids = []
     recommended_review_ids = []
-    for (review_id, index) in review_index_dict.items():
-        for tup in list_of_tups:
-            if index == int(tup[0]):
+    for l in list_of_lists:
+        liked_index = l[0][0]
+        rec_indices = []
+        rec_ids = []
+        for tup in l:
+            rec_indices.append(tup[1]) 
+        for (review_id, index) in review_index_dict.items():
+            if index == liked_index:
                 liked_review_ids.append(review_id)
-            elif index == int(tup[1]):
-                recommended_review_ids.append(review_id)
+            elif index in rec_indices:
+                rec_ids.append(review_id)
+        recommended_review_ids.append(rec_ids)
+
     liked_businesses = []
-    recommended_businesses = []
+    rec_businesses = []
     for review_id in liked_review_ids:
         business_id = business_id_dict[review_id]
         business_name = business_name_dict[business_id]
         liked_businesses.append(business_name)
-    for review_id in recommended_review_ids:
-        business_id = business_id_dict[review_id]
-        business_name = business_name_dict[business_id]
-        recommended_businesses.append(business_name)
-    for i in liked_businesses:
-        ind = liked_businesses.index(i)
-        s = 'Because you liked {}, we think you should try {}.'.format(\
-            i, recommended_businesses[ind])
+    for review_id_list in recommended_review_ids:
+        business_name_list = []
+        for review_id in review_id_list:
+            business_id = business_id_dict[review_id]
+            business_name = business_name_dict[business_id]
+            business_name_list.append(business_name)
+        rec_businesses.append(business_name_list)
+
+    for business in liked_businesses:
+        rec_list = rec_businesses[liked_businesses.index(business)]
+        s_recommend = ''
+        if num_recs > 1:
+            for ind in range(0, len(rec_list) - 1):
+                s_recommend = s_recommend + rec_list[ind] + ', '
+            s_recommend = s_recommend + 'and ' + rec_list[len(rec_list) - 1]
+        else:
+            s_recommend = rec_list[0]
+        s = 'Because you liked {}, we think you should try {}.'.format(business, s_recommend)
         print(s)
+
+
 
 def go(user_id, tfidf_file, num_recs): 
     rec_list = create_rec_list(user_id, tfidf_file, num_recs)

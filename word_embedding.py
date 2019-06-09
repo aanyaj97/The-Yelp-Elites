4dafe7bf7720e18f9f7c2203336f45476d235d63
shyamsundersriram <shyamsundersriram@outlook.com>
2019-06-09 18:22:14 -0500

    code comments word embedding

--- word_embedding.py
+++ word_embedding.py
@@ -1,10 +1,12 @@
 import numpy as np 
 from scipy import spatial
 
+
+# This is the saved word_embedding matrix computed using MPI 
 with np.load('matrix_W.npz') as data:
     W = data['arr_0']
 
-# Saved vocabulary as json file
+# Saved vocabulary as json file. Vocab has only 20K words. 
 import json
 vocab_file = open('Small_Dataset_Processing/vocab_ind20.json')
 vocab_str = vocab_file.read()
@@ -15,8 +17,21 @@ for word in vocab.keys():
     index = vocab[word]
     index_to_word[index] = word
 
+
 def close_words(word, num_words):
-    if (word not in vocab or vocab[word] > 9999): 
+    '''
+    Function that prints words that are similar to each other. Note it only 
+    takes the first 10000 words to limit the size of matrix. Can be 
+    done with larger vocab sizes. 
+
+    Inputs: 
+      word: (string) the word you want to compare in question
+      num_words: (int) the number of comparison words you want 
+
+    
+    ''' 
+
+    if (word not in vocab or vocab[word] > 9999): #modify condition with larger vocab size
         print('word not in vocabulary')
         return None 
     d = np.shape(W)[0]
@@ -30,6 +45,9 @@ def close_words(word, num_words):
         print(w)
 
 def print_words(word_list, num_words): 
+    '''
+    Calls function repeatedly for a list of words to be compared. 
+    ''' 
     for word in word_list: 
         print("These are closest words to: " + word)
         close_words(word, num_words)
@@ -37,6 +55,24 @@ def print_words(word_list, num_words):
 
 
 def analogy_words(word_tup, k): 
+    '''
+    Finds relevant analogies of words in the vocabulary. Excellent for 
+    testing words of sentiment. 
+
+    Input: word_tup tuple of strings in form (word0, word1, word2) 
+     
+    Note word0 and word1 are related and the function tries to find 
+    related words for word2 by computing the nearest embedding 
+    vectors for v = v_word1 - v_word0 + v_word2. 
+
+    Sample input (awesome, phenomenal, bad)
+    Output: 
+        horrible 
+        terrible 
+        disappointing 
+        poor 
+        awful 
+    ''' 
     for word in word_tup:
         if (word not in vocab or vocab[word] > 9999): 
             print(word + ' not in vocabulary')
@@ -48,7 +84,7 @@ def analogy_words(word_tup, k):
         index = vocab[w]
         index_to_word[index] = w
     d = np.shape(W)[0]
-    vec = W[vocab[word_tup[1]]] - W[vocab[word_tup[0]]] + W[vocab[word_tup[2]]]
+    vec = W[vocab[word_tup[1]]] - W[vocab[word_tup[0]]] + W[vocab[word_tup[2]]] 
     W_hat = W - vec
     norms = np.sum(W_hat**2, axis=1)
     indices = norms.argsort()[:k + 3]
@@ -62,11 +98,6 @@ def analogy_words(word_tup, k):
             print(w)
         w_index += 1 
 
-# def print_analogies(analogy_list, vocab, k): 
-#     for word_tup in analogy_list:
-#         print("If " + word_tup[0] + ":" + word_tup[1] + " then " + word_tup[2] + ":" )
-#         analogy_words(word_tup, vocab, k)
-#         print("\n")
 
 
     
